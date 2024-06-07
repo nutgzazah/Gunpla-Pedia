1b6958e1bfcaf1a1e30a9698530e375224104936
JaTingJA <55779319+nutgzazah@users.noreply.github.com>
2024-06-07 07:20:10 +0700

    Login page

--- client/src/components/pages/auth/Login.js
+++ client/src/components/pages/auth/Login.js
@@ -1,161 +1,82 @@
 import * as React from 'react';
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import CssBaseline from '@mui/material/CssBaseline';
-import TextField from '@mui/material/TextField';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
-import Link from '@mui/material/Link';
-import Paper from '@mui/material/Paper';
-import Box from '@mui/material/Box';
-import Grid from '@mui/material/Grid';
-import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
-import Typography from '@mui/material/Typography';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
-
-//funtion
-import { login } from '../../../functions/auth';
 import { useNavigate } from 'react-router-dom';
-
-import { useDispatch } from "react-redux";
-import { login as loginRedux} from "../../../store/userSlice"
-
-function Copyright(props) {
-  return (
-    <Typography variant="body2" color="text.secondary" align="center" {...props}>
-      {'Copyright © '}
-      <Link color="inherit" href="https://mui.com/">
-        Your Website
-      </Link>{' '}
-      {new Date().getFullYear()}
-      {'.'}
-    </Typography>
-  );
-}
-
-// TODO remove, this demo shouldn't need to reset the theme.
+import { useDispatch } from 'react-redux';
+import { login as loginRedux } from '../../../store/userSlice';
+import { login } from '../../../functions/auth';
+import { MdEmail } from 'react-icons/md';
+import { IoIosLock } from 'react-icons/io';
+import { Link } from 'react-router-dom';
+import './Login.css'; // Assuming you copied your friend's CSS file
 
 const defaultTheme = createTheme();
 
-export default function Login() {
-  const navi = useNavigate()
-  const dispatch = useDispatch()
+function Login() {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
 
   const handleSubmit = (event) => {
     event.preventDefault();
     const data = new FormData(event.currentTarget);
 
-    const formrgt={
-        name:data.get("name"),
-        password:data.get("password"),
-    }
+    const formrgt = {
+      name: data.get('name'),
+      password: data.get('password'),
+    };
+
     login(formrgt)
-    .then(res=>{
-        console.log(res)
-        alert(res.data)
-        dispatch(loginRedux({
-          name:res.data.payload.user.name,
-          role:res.data.payload.user.role,
-          token:res.data.token
-        }));
-        localStorage.setItem('token',res.data.token);
-        roleRedirects(res.data.payload.user.role)
-    }).catch(err=>console.log(err))
+      .then((res) => {
+        console.log(res);
+        alert(res.data);
+        dispatch(
+          loginRedux({
+            name: res.data.payload.user.name,
+            role: res.data.payload.user.role,
+            token: res.data.token,
+          })
+        );
+        localStorage.setItem('token', res.data.token);
+        roleRedirects(res.data.payload.user.role);
+      })
+      .catch((err) => console.log(err));
   };
 
-
-  const roleRedirects = (role) =>{
-    if(role === 'admin'){
-      navi("/admin/index")
-    }else{
-      navi("/user/index")
+  const roleRedirects = (role) => {
+    if (role === 'admin') {
+      navigate('/admin/index');
+    } else {
+      navigate('/user/index');
     }
-  }
+  };
 
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Grid container component="main" sx={{ height: '100vh' }}>
-        <CssBaseline />
-        <Grid
-          item
-          xs={false}
-          sm={4}
-          md={7}
-          sx={{
-            // backgroundImage: 'url(https://source.unsplash.com/random?wallpapers)',
-            backgroundImage: 'url(/assets/wallpaper.png)',
-            backgroundRepeat: 'no-repeat',
-            backgroundColor: (t) =>
-              t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
-            backgroundSize: 'cover',
-            backgroundPosition: 'center',
-          }}
-        />
-        <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
-          <Box
-            sx={{
-              my: 8,
-              mx: 4,
-              display: 'flex',
-              flexDirection: 'column',
-              alignItems: 'center',
-            }}
-          >
-            <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-              <LockOutlinedIcon />
-            </Avatar>
-            <Typography component="h1" variant="h5">
-              Login
-            </Typography>
-            <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 1 }}>
-              <TextField
-                margin="normal"
-                required
-                fullWidth
-                id="name"
-                label="Username"
-                name="name"
-                autoFocus
-              />
-              <TextField
-                margin="normal"
-                required
-                fullWidth
-                name="password"
-                label="Password"
-                type="password"
-                id="password"
-                autoComplete="current-password"
-              />
-              <FormControlLabel
-                control={<Checkbox value="remember" color="primary" />}
-                label="Remember me"
-              />
-              <Button
-                type="submit"
-                fullWidth
-                variant="contained"
-                sx={{ mt: 3, mb: 2 }}
-              >
-                Login
-              </Button>
-              <Grid container>
-                <Grid item xs>
-                  <Link href="#" variant="body2">
-                    Forgot password?
-                  </Link>
-                </Grid>
-                <Grid item>
-                  <Link href="#" variant="body2">
-                    {"Don't have an account? Sign Up"}
-                  </Link>
-                </Grid>
-              </Grid>
-              <Copyright sx={{ mt: 5 }} />
-            </Box>
-          </Box>
-        </Grid>
-      </Grid>
+      <div className="login">
+        <div className="wrapper">
+          <form onSubmit={handleSubmit}>
+            <h1>Login</h1>
+            <div className="input-box">
+              <input type="text" name="name" placeholder="Username" required />
+              <i><MdEmail /></i>
+            </div>
+            <div className="input-box">
+              <input type="password" name="password" placeholder="Password" required />
+              <i><IoIosLock /></i>
+            </div>
+            <div className="remember-forget">
+              <label>
+                <input type="checkbox" /> Remember me
+              </label>
+            </div>
+            <button type="submit" className="btn">Login</button>
+            <div className="register-link">
+              <p>Don't have an account? <Link to="/signup">Register</Link></p>
+            </div>
+          </form>
+        </div>
+      </div>
     </ThemeProvider>
   );
 }
+
+export default Login;
