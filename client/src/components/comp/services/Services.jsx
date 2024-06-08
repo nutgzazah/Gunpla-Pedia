fbb8da57f73a72b27c37f93c98a46481fdcccb8f
ณัฏฐสิทธิ์ ตันนิติไพศาล <94214122+nuttas@users.noreply.github.com>
2024-06-08 22:45:56 +0700

    Final home page & All gunpla in collection

--- client/src/components/comp/services/Services.jsx
+++ client/src/components/comp/services/Services.jsx
@@ -13,24 +13,27 @@ const Services = () => {
             <div className='services-box'>
                 <IoIosInformationCircle style={{ fontSize: '7rem' }}/>
                 <h3>Gunpla Information</h3>
-                <p> Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ut sequi, rem magnam nesciunt minima placeat, 
-                    doloribus non numquam. Explicabo, laboriosam nisi reprehenderit?</p>
+                <p> There is complete detailed information on each Gunpla model. What sector is it in? A product description that includes technical information such as size, 
+                  grade, and release date. Features highlighting the strengths of that Gunpla model and its 
+                  advantages and disadvantages, which is a review of the quality of the parts. Difficulty in assembly structural strength, etc.</p>
                 <a href='#' className='btn-services'>See More</a>
             </div>
 
             <div className='services-box'>
                 <BsCollectionFill style={{ fontSize: '7rem' }}/>
                 <h3>Builder Collection</h3>
-                <p> Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ut sequi, rem magnam nesciunt minima placeat, 
-                    doloribus non numquam. Explicabo, laboriosam nisi reprehenderit?</p>
+                <p>The collection save feature allows users to add Gunpla they own to their personal collections. There is also a collection management feature that allows users to sort, edit, or 
+                  delete Gunpla in their personal collections according to their preferences. 
+                  and a collection sharing feature that allows users to share their collections with friends. or in the community of people interested in Gunpla as well</p>
                 <a href='#' className='btn-services'>See More</a>
             </div>
 
             <div className='services-box'>
                 <FaPaintBrush style={{ fontSize: '7rem' }}/>
                 <h3>A&P Techniques</h3>
-                <p> Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ut sequi, rem magnam nesciunt minima placeat, 
-                    doloribus non numquam. Explicabo, laboriosam nisi reprehenderit?</p>
+                <p> Assembly: Cutting Techniques  Using tools and assembling various parts Make it tight and form-fitting.
+Painting: Choosing colors Techniques for spray painting, brushing, using Masking Tape, applying lacquer, and weathering to increase realism.
+Additional decorations: Decal, panel lining and other customizations</p>
                 <a href='#' className='btn-services'>See More</a>
             </div>
         </div>
--- client/src/components/pages/collection/Collection.jsx
+++ client/src/components/pages/collection/Collection.jsx
@@ -1,11 +1,70 @@
-import React from 'react'
+import React, { useState } from 'react';
 import Topcollect from './topcollect/Topcollect'
+import Gsearch from '../../comp/gsearch/Gsearch'
+import Guidegrade from '../../comp/guidegrade/Guidegrade'
+import Product from '../../comp/showgun/Product'
+import products from '../../db/data'
+import Card from '../../comp/showcom/Cards';
 
 const Collection = () => {
+  const [selectedSeries, setSelectedSeries] = useState(null);
+  const [query, setQuery] = useState("");
+
+  //input filter
+  const handleInputChange = event => {
+    setQuery(event.target.value);
+  };
+
+  const filteredItems = products.filter((product) => 
+   product.name.toLocaleLowerCase().indexOf(query.toLocaleLowerCase()) !== -1
+  );
+
+  //radio filter
+  const handleChange = event => {
+    setSelectedSeries(event.target.value);
+  };
+
+  //buttons filter
+  const handleClick = event => {
+    setSelectedSeries(event.target.value);
+  };
+
+  function filteredData(products, selected, query) {
+    let filteredProducts = products;
+
+    // Filtering Input Items
+    if (query) {
+      filteredProducts = filteredItems;
+    }
+
+    // Applying selected filter
+    if (selected) {
+      filteredProducts = filteredProducts.filter(
+        ({ serie, grade, name }) =>
+          serie === selected ||
+          grade === selected ||
+          name === selected
+      );
+    }
+
+    return filteredProducts.map(({img, name, star, totalrating }) => (
+      <Card 
+      key = {Math.random()}
+      img = {img}
+      name = {name}
+      star = {star}
+      totalrating = {totalrating}
+      />
+    ))
+  }
+
+  const result = filteredData(products, selectedSeries, query);
   return (
     <div>
       <Topcollect />
-      
+      <Gsearch query={query} handleInputChange={handleInputChange}/>
+      <Guidegrade handleClick={handleClick}/>
+      <Product result={result} handleChange={handleChange}  />
     </div>
   )
 }
