41b706149af981c8964d2df297794c27b732f0e5
JaTingJA <55779319+nutgzazah@users.noreply.github.com>
2024-06-09 14:23:36 +0700

    updateRoute

--- client/src/routes/AdminRoute.js
+++ client/src/routes/AdminRoute.js
@@ -1,41 +1,43 @@
-import React,{ useEffect, useState } from 'react'
-import SideBar from '../layout/SideBar'
-import HeaderBar from '../layout/HeaderBar'
-import { Box } from '@mui/material'
-import { Routes } from 'react-router-dom'
-import { useSelector } from "react-redux"
-import { currentAdmin } from '../functions/auth'
+import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useSelector } from "react-redux";
+import { currentAdmin } from '../functions/auth';
+import SideBar from '../layout/SideBar';
+import HeaderBar from '../layout/HeaderBar';
+import { Box } from '@mui/material';
 
+const AdminRoute = ({ children }) => {
+  const navigate = useNavigate();
+  const { user } = useSelector((state) => ({ ...state }));
+  const [ok, setOk] = useState(false);
 
-const AdminRoute = ({children}) => {
-  const { user } = useSelector((state)=>({...state}))
-  const[ok, setOk] = useState(false)
-  
-  useEffect(()=>{
-    if(user && user.user.token){
-      currentAdmin(user.user.token).then(r=>{
-        // console.log(r)
-        setOk(true)
-      }).catch((err)=> {
-        console.log(err)
-        setOk(false)
-      })
+  useEffect(() => {
+    // Check if user is not logged in, then navigate to /login
+    if (!user || !user.user || !user.user.token) {
+      navigate('/login');
+    } else {
+      // Check if the user is an admin
+      currentAdmin(user.user.token).then(response => {
+        setOk(true);
+      }).catch((err) => {
+        console.log(err);
+        navigate('/gunpla');
+      });
     }
-  },[user])
-
+  }, [user, navigate]);
 
   return ok ? (
     <div className="app">
-    <SideBar/>
-    <main className="content">
-      <div className="content_body">
-        <Box m="20px">
+      <SideBar />
+      <main className="content">
+        <div className="content_body">
+          <Box m="20px">
             {children}
-        </Box>
-      </div>
-    </main>
+          </Box>
+        </div>
+      </main>
     </div>
-  ) : <h1>NO PERMISSION</h1>
+  ) : null; // Render null or a loading spinner while checking permissions
 }
 
-export default AdminRoute
+export default AdminRoute;
--- client/src/routes/UserRoute.js
+++ client/src/routes/UserRoute.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react';
 import { useSelector } from 'react-redux'
 import { useNavigate } from 'react-router-dom';
 
@@ -8,12 +8,18 @@ const UserRoute = ({children}) => {
   console.log('UserRoute',user)
     //check if user login yet
     
+    useEffect(() => {
+      // Check if user is not logged in, then navigate to /login
+      if (!user || !user.user || !user.user.token) {
+        navigate('/login');
+      }
+    }, [user, navigate]);
 
-  return user && user.user.token 
-  ? <>
-  { children }
-  </>
-   : <h1>No Login</h1>
-}
+  return user && user.user && user.user.token ? (
+    <>
+      {children}
+    </>
+  ) : null; // Return null or a loading spinner if you prefer while redirecting
+};
 
 export default UserRoute
